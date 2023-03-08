375562affbe52b1181ca2e678f18cc22c6eadbdb
Gaël Thomas <sarah_khoshaghebat@yahoo.com>
2023-03-08 11:24:44 +0330

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+const display =document.querySelector("#display");
+const buttons = document.querySelector("button");
+
+button.forEach(btn => {
+    btn.addEventLisener("click",()=> {
+        if(btn.id === "=") {
+            display.value = eval(display.value);
+        } else if (btn.id === "ac") {
+            display.value = "";
+        } else if (btn.id == "de"){
+            display.value = display.value.slice(0,-1);
+        } else {
+            display.value += btn.id;
+        }
+    });
+});
